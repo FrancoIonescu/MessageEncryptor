12190fb020d6b747840fb37cc76b4676a9344376
Franco Ionescu <francoionescu@outlook.com>
2025-04-03 11:30:06 +0300

    Save encrypted message and password to file

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Introduceti mesajul de criptat: ");
+        System.out.print("Enter the message to encrypt: ");
         Scanner scanner = new Scanner(System.in);
-        String mesaj = scanner.nextLine();
+        String message = scanner.nextLine();
 
-        String mesajCriptat = AlgoritmCriptare.cripteazaMesaj(mesaj);
-        System.out.println("Mesajul criptat este: " + mesajCriptat);
+        System.out.print("Enter the password: ");
+        String password = scanner.nextLine();
+        String encryptedPassword = EncryptionAlgorithm.encryptMessage(password);
 
-        String mesajDecriptat = AlgoritmCriptare.decripteazaMesaj(mesajCriptat);
-        System.out.println("Mesajul decriptat este: " + mesajDecriptat);
+        String encryptedMessage = EncryptionAlgorithm.encryptMessage(message);
 
+        creareFisier(encryptedPassword, encryptedMessage);
+
+    }
+
+    public static void creareFisier(String parolaCriptata, String mesajCriptat) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("EncryptedMessage.txt"));
+            writer.write(parolaCriptata + ' ');
+            writer.write(mesajCriptat);
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
