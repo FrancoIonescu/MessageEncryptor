e921801380ef5842a73aea4012f4fc737187b3a8
Franco Ionescu <francoionescu@outlook.com>
2025-04-03 21:29:36 +0300

    Decrypt the file by typing the correct file password

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String message = scanner.nextLine();
 
-        System.out.print("Enter the password: ");
-        String password = scanner.nextLine();
-        String encryptedPassword = EncryptionAlgorithm.encryptMessage(password);
+        System.out.print("Enter the password for encryption: ");
+        String passwordEncrypt = scanner.nextLine();
+        String encryptedPassword = EncryptionAlgorithm.encryptMessage(passwordEncrypt);
 
         String encryptedMessage = EncryptionAlgorithm.encryptMessage(message);
 
-        creareFisier(encryptedPassword, encryptedMessage);
+        EncryptionAlgorithm.createFile(encryptedPassword, encryptedMessage);
 
-    }
+        System.out.print("Enter the password for decryption: ");
+        String passwordDecrypt = scanner.nextLine();
+        EncryptionAlgorithm.requestDecryptMessage(passwordDecrypt, "EncryptedMessage.txt");
 
-    public static void creareFisier(String parolaCriptata, String mesajCriptat) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("EncryptedMessage.txt"));
-            writer.write(parolaCriptata + ' ');
-            writer.write(mesajCriptat);
-            writer.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
